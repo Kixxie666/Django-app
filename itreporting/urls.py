4f9dd7f0e2d2dc8648794cc96fd6565b13d23b53
kixxie666 <kixxie6@outlook.com>
2024-11-06 12:17:33 +0000

    Main bulk

--- itreporting/urls.py
+++ itreporting/urls.py
@@ -1,9 +1,15 @@
-from . import views
-from django.urls import path
+from django.urls import path, include
+from . import views  
+from users import views as user_views
+
 
 app_name = 'itreporting'
 
 urlpatterns = [
-    path('', views.home, name = 'home')
-]
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('profile/', views.profile, name='profile'),
+    path('register/', user_views.register, name='register'),
 
+]
