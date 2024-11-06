fe5dd23188371ce16b6e744118b6a7a0e8569edf
kixxie666 <kixxie6@outlook.com>
2024-11-06 12:17:33 +0000

    Main bulk

--- itreporting/views.py
+++ itreporting/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    
     return render(request, 'itreporting/home.html', {'title': 'Welcome'})
+
+def about(request):
+    return render(request, 'itreporting/about.html', {'title': 'About'})
+
+def profile(request):
+    return render(request, 'users/profile.html', {'title': 'profile'})
+
+def contact(request):
+    return render(request, 'itreporting/contact.html', {'title': 'Contact'})
+
